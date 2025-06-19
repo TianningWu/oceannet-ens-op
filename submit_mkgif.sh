dbc37fd84ce5c655cd31a337694fcd448a543b12
Tianning Wu <twu27@ncsu.edu>
2025-06-19 12:22:10 -0500

    add a shell job script for running python script to make gif animations

---
+++ submit_mkgif.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -p ai2es
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --chdir=./
+#SBATCH --job-name="MKGIF"
+#SBATCH --output=./logs/log_%x.out
+#SBATCH --error=./logs/log_%x.err
+
+#  source my python env
+module purge
+source /home/twu27/.bashrc
+source /home/twu27/python3/env/oceannet/bin/activate
+module load Python/3.10.8-GCCcore-12.2.0
+hostname
+start_time=`date +%s`
+
+python -u mkvid_standalone.py
+
+end_time=`date +%s`
+echo execution time was `expr $end_time - $start_time` s.
