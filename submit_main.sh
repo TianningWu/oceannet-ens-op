23e77bb5a6979b222ecf0c874781d8631ea90680
TianningWu <twu27@ncsu.edu>
2026-07-10 09:21:57 -0400

    explict path dir

--- submit_main.sh
+++ submit_main.sh
@@ -12,9 +12,12 @@
 hostname
 start_time=`date +%s`
 
+mercator_dir=/ourdisk/hpc/ai2es/twu27/srcdata/Mercator_hourly
+ens_pred_dir=/ourdisk/hpc/ai2es/twu27/oceannet2/ens_pred_op
+
 # Submit download 
-cd /ourdisk/hpc/ai2es/twu27/srcdata/Mercator_hourly
-jid1=$(sbatch --parsable submit_download_prep_mercator_nrt.sh)
+cd "$mercator_dir"
+jid1=$(sbatch --parsable --chdir="$mercator_dir" submit_download_prep_mercator_nrt.sh)
 echo "Submitted Mercator download with Job ID $jid1" | ts
 sacct -j $jid1 --format=State --noheader | grep -q COMPLETED
 while [ $? -ne 0 ]; do
@@ -24,8 +27,8 @@ done
 echo "Mercator download completed." | ts
 
 # Submit prediction
-cd /ourdisk/hpc/ai2es/twu27/oceannet2/ens_pred_op
-jid2=$(sbatch --parsable submit_prediction_120days.sh)
+cd "$ens_pred_dir"
+jid2=$(sbatch --parsable --chdir="$ens_pred_dir" submit_prediction_120days.sh)
 echo "Submitted prediction with Job ID $jid2" | ts
 sacct -j $jid2 --format=State --noheader | grep -q COMPLETED
 while [ $? -ne 0 ]; do
@@ -35,8 +38,8 @@ done
 echo "Prediction completed." | ts
 
 # Submit combine NetCDF
-cd /ourdisk/hpc/ai2es/twu27/oceannet2/ens_pred_op
-jid3=$(sbatch --parsable submit_combine_nc.sh)
+cd "$ens_pred_dir"
+jid3=$(sbatch --parsable --chdir="$ens_pred_dir" submit_combine_nc.sh)
 echo "Submitted combine ensemble NetCDF with Job ID $jid3" | ts
 sacct -j $jid3 --format=State --noheader | grep -q COMPLETED
 while [ $? -ne 0 ]; do
@@ -46,8 +49,8 @@ done
 echo "Combine ensemble NetCDF completed." | ts
 
 # Submit plot
-cd /ourdisk/hpc/ai2es/twu27/oceannet2/ens_pred_op
-jid4=$(sbatch --parsable submit_plot_all.sh)
+cd "$ens_pred_dir"
+jid4=$(sbatch --parsable --chdir="$ens_pred_dir" submit_plot_all.sh)
 echo "Submitted plot with Job ID $jid4" | ts
 sacct -j $jid4 --format=State --noheader | grep -q COMPLETED
 while [ $? -ne 0 ]; do
@@ -57,8 +60,8 @@ done
 echo "Plot completed." | ts
 
 # Submit nc for visualization
-cd /ourdisk/hpc/ai2es/twu27/oceannet2/ens_pred_op
-jid5=$(sbatch --parsable submit_nc4vis.sh)
+cd "$ens_pred_dir"
+jid5=$(sbatch --parsable --chdir="$ens_pred_dir" submit_nc4vis.sh)
 echo "Submitted make nc for visualization with Job ID $jid5" | ts
 sacct -j $jid5 --format=State --noheader | grep -q COMPLETED
 while [ $? -ne 0 ]; do
